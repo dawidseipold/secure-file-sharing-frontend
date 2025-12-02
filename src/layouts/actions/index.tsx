233166df0f94a47d5c91d9297abcfb8f366e601f
dawidseipold <seipold.dawid@gmail.com>
2025-12-03 00:20:20 +0100

    chore: update actions layout to match the new store

--- src/layouts/actions/index.tsx
+++ src/layouts/actions/index.tsx
@@ -1,17 +1,25 @@
-import {Outlet, type RouteComponent} from "@tanstack/react-router";
-import {H1, Lead} from "@/components/typography";
-import {useActionsLayoutStore} from "@/stores/actionsLayoutStore.ts";
+import { Outlet, type RouteComponent } from "@tanstack/react-router"
+import { H3, Lead } from "@/components/typography"
+import { useActionsLayoutStore } from "@/stores/actionsLayoutStore.ts"
+import { PageTooltip } from "@/components/ui/page/page-tooltip.tsx"
 
 export const ActionsLayout: RouteComponent = () => {
-    const title = useActionsLayoutStore((s) => s.title);
-    const subtitle = useActionsLayoutStore((s) => s.subtitle);
+    const title = useActionsLayoutStore((s) => s.title)
+    const subtitle = useActionsLayoutStore((s) => s.subtitle)
+    const tooltip = useActionsLayoutStore((s) => s.tooltip)
 
     return (
         <div>
-            <H1>{title}</H1>
-            <Lead>{subtitle}</Lead>
+            <div>
+                <div className={"flex items-center gap-2"}>
+                    <H3>{title}</H3>
+                    <PageTooltip text={tooltip} />
+                </div>
 
-            <Outlet/>
+                <Lead>{subtitle}</Lead>
+            </div>
+
+            <Outlet />
         </div>
     )
 }
