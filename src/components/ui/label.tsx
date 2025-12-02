b5b422c48cbceea6712b6f402d92fd9b1ccf566b
dawidseipold <seipold.dawid@gmail.com>
2025-12-02 22:52:04 +0100

    fix: replace unexisting cva variants with tailwind-variants ones in button component chore: align label and input components to fit the rest of the components

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,13 +2,16 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 
 import { cn } from "@/utils/styles"
 import type { ComponentPropsWithoutRef, FC } from "react"
-import { tv } from "tailwind-variants"
+import { tv, type VariantProps } from "tailwind-variants"
 
 const labelVariants = tv({
     base: "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
 })
 
-interface LabelProps extends ComponentPropsWithoutRef<typeof LabelPrimitive.Root> {
+interface LabelProps
+    extends
+        ComponentPropsWithoutRef<typeof LabelPrimitive.Root>,
+        VariantProps<typeof labelVariants> {
     className?: string
 }
 
@@ -16,7 +19,7 @@ export const Label: FC<LabelProps> = ({ className, ...props }) => {
     return (
         <LabelPrimitive.Root
             data-slot="label"
-            className={cn(labelVariants({ className }))}
+            className={cn(labelVariants({}), className)}
             {...props}
         />
     )
